0bcd9565ae17344749c96aa0611ddfb2d49d13a1
Marcos Martini <martini.bnu@gmail.com>
2021-08-29 18:50:15 -0300

    Till validation and error handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -37,7 +37,15 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find((task) => task.id === id);
+        // Try to get task
+        const found = this.tasks.find((task) => task.id === id);
+        // If not found, throw an error (404 Not Found)
+        if(!found) {
+            throw new NotFoundException(`Task with ID "${id}" not found!`);
+        }
+        // Otherwise return the found task
+        return found;
+
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -53,7 +61,8 @@ export class TasksService {
     }
 
     deleteTaskById(id: string): void {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter((task) => task.id !== found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
