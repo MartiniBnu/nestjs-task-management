72d252ac6729dad8a2338aea7edd627c7823ca61
Marcos Martini <martini.bnu@gmail.com>
2021-08-31 22:25:30 -0300

    Logs

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -18,10 +18,12 @@ import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
+import { Logger } from '@nestjs/common';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TasksController');
   constructor(private tasksService: TasksService) {}
 
   @Get()
@@ -29,6 +31,7 @@ export class TasksController {
     @Query() filterDto: GetTasksFilterDto,
     @GetUser() user: User,
   ): Promise<Task[]> {
+    this.logger.verbose(`User "${user.username}" retrieving all tasks with the filters: "${JSON.stringify(filterDto)}"`);
     return this.tasksService.getTasks(filterDto, user);
   }
 
@@ -42,6 +45,7 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(`User "${user.username}" is creating a new task with data: "${JSON.stringify(createTaskDto)}"`);
     return this.tasksService.createTask(createTaskDto, user);
   }
 
